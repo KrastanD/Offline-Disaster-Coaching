b8543b9fef011cfcfcaba007848dab9a3c6c66f1
Krastan Dimitrov <krastan.kdd@gmail.com>
2017-11-04 23:47:21 -0500

    Added Json and set up basic form.

--- Offline-Disaster-Coaching/Offline-Disaster-Coaching/Form1.Designer.cs
+++ Offline-Disaster-Coaching/Offline-Disaster-Coaching/Form1.Designer.cs
@@ -28,12 +28,61 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.htmlLabel1 = new MetroFramework.Drawing.Html.HtmlLabel();
+            this.metroComboBox1 = new MetroFramework.Controls.MetroComboBox();
+            this.SuspendLayout();
+            // 
+            // htmlLabel1
+            // 
+            this.htmlLabel1.AutoScrollMinSize = new System.Drawing.Size(112, 18);
+            this.htmlLabel1.AutoSize = false;
+            this.htmlLabel1.BackColor = System.Drawing.SystemColors.Window;
+            this.htmlLabel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.htmlLabel1.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.htmlLabel1.Location = new System.Drawing.Point(132, 81);
+            this.htmlLabel1.Name = "htmlLabel1";
+            this.htmlLabel1.Size = new System.Drawing.Size(234, 44);
+            this.htmlLabel1.TabIndex = 0;
+            this.htmlLabel1.Text = "What is your current situation?";
+            this.htmlLabel1.Click += new System.EventHandler(this.htmlLabel1_Click_1);
+            // 
+            // metroComboBox1
+            // 
+            this.metroComboBox1.FormattingEnabled = true;
+            this.metroComboBox1.ItemHeight = 24;
+            this.metroComboBox1.Items.AddRange(new object[] {
+            "Earthquakes",
+            "Flood",
+            "Hurricane",
+            "Tornado"});
+            this.metroComboBox1.Location = new System.Drawing.Point(176, 143);
+            this.metroComboBox1.Name = "metroComboBox1";
+            this.metroComboBox1.Size = new System.Drawing.Size(121, 30);
+            this.metroComboBox1.TabIndex = 1;
+            this.metroComboBox1.UseSelectable = true;
+            this.metroComboBox1.SelectedIndexChanged += new System.EventHandler(this.metroComboBox1_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(4F, 7F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(506, 329);
+            this.Controls.Add(this.metroComboBox1);
+            this.Controls.Add(this.htmlLabel1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 4.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(2, 1, 2, 1);
+            this.Name = "Form1";
+            this.Padding = new System.Windows.Forms.Padding(10, 26, 10, 9);
+            this.Text = "Offline Disaster Coaching";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private MetroFramework.Drawing.Html.HtmlLabel htmlLabel1;
+        private MetroFramework.Controls.MetroComboBox metroComboBox1;
     }
 }
 
--- Offline-Disaster-Coaching/Offline-Disaster-Coaching/Program.cs
+++ Offline-Disaster-Coaching/Offline-Disaster-Coaching/Program.cs
@@ -17,6 +17,8 @@ namespace Offline_Disaster_Coaching
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+
+
         }
     }
 }
