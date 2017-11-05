5eaa9920a15361c73e74ed63d0e736f68f28fe9c
Robert Beckwith <rjbeckwith55@gmail.com>
2017-11-05 00:44:22 -0500

    added second form

---
+++ Offline-Disaster-Coaching/Offline-Disaster-Coaching/Form1 - Copy.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MetroFramework.Forms;
+using Newtonsoft.Json;
+
+
+namespace Offline_Disaster_Coaching
+{
+
+    public partial class Form1 : MetroForm
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+        private void htmlLabel1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void metroComboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if(metroComboBox1.Text == "Earthquakes")
+            {
+                
+            }
+        }
+    }
+}
